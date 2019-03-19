2bbadcff9526fccab772b0c7c38fa3198724fa71
TheGregTanaka <gregoryftanaka@gmail.com>
2019-03-19 01:02:40 -0600

    update: Through module 10

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,53 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { IProduct } from "./product";
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
 	selector: 'pm-products',
 	templateUrl: './product-list.component.html',
 	styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
 	pageTitle: string = 'Product List';
 	imageWidth: number = 50;
 	imageMargin: number = 2;
 	showImage: boolean = false;
+	errorMessage: string;
 
 	_listFilter: string;
 	get listFilter(): string {
 		return this._listFilter;
 	}
-	set listFilter(value:string) {
+	set listFilter(value: string) {
 		this._listFilter = value;
 		this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
 	}
-	
+
 	filteredProducts: IProduct[];
 	products: IProduct[] = [
-		{
-			"productId": 2,
-			"productName": "Garden Cart",
-			"productCode": "GDN-0023",
-			"releaseDate": "March 18, 2016",
-			"description": "15 gallon capacity rolling garden cart",
-			"price": 32.99,
-			"starRating": 4.2,
-			"imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-		  },
-		  {
-			"productId": 5,
-			"productName": "Hammer",
-			"productCode": "TBX-0048",
-			"releaseDate": "May 21, 2016",
-			"description": "Curved claw steel hammer",
-			"price": 8.9,
-			"starRating": 4.8,
-			"imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-		  }
 	];
 
-	constructor() {
-		this.filteredProducts = this.products;
-		this.listFilter = 'cart';
+	constructor(private productService: ProductService) {
 	}
 
 	onRatingClicked(message: string): void {
@@ -55,7 +35,7 @@ export class ProductListComponent implements OnInit{
 	}
 	performFilter(filterBy: string): IProduct[] {
 		filterBy = filterBy.toLocaleLowerCase();
-		return this.products.filter((product: IProduct) => 
+		return this.products.filter((product: IProduct) =>
 			product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
 	}
 	toggleImage(): void {
@@ -63,6 +43,12 @@ export class ProductListComponent implements OnInit{
 	}
 
 	ngOnInit(): void {
-		console.log('In OnInit');
+		this.productService.getProducts().subscribe(
+			products => {
+				this.products = products;
+				this.filteredProducts = this.products;
+			},
+			error => this.errorMessage = <any>error
+		);
 	}
 }
