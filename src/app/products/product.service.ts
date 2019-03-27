defec4a8c5527b3a24d39f3135ae3b4c4dac789f
TheGregTanaka <gregoryftanaka@gmail.com>
2019-03-26 21:54:19 -0600

    through 11, but I think I missed routing

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,37 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProductService {
-	private productUrl = 'api/products/products.json';
+  private productUrl = 'api/products/products.json';
 
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-	getProducts(): Observable<IProduct> {
-		return this.http.get<IProduct[]>(this.productUrl).pipe(
-			tap(data => console.log('All: ' + JSON.stringify(data))),
-			catchError(this.handleError)
-		);
-	}
+  getProducts(): Observable<IProduct> {
+      return this.http.get<IProduct[]>(this.productUrl).pipe(
+          tap(data => console.log('All: ' + JSON.stringify(data))),
+          catchError(this.handleError)
+      );
+  }
 
-	private handleError(err: HttpErrorResponse) {
-		// irl we would log this properly
-		let errorMessage = '';
-		if (err.error instanceof ErrorEvent) {
-			// client side network error
-			errorMessage = `And error occurred: ${err.error.message}`;
-		} else {
-			// backend returned unsuccessful code
-			// body may contain clues
-			errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-		}
-		console.error(errorMessage);
-		return throwError(errorMessage);
-	}
+  private handleError(err: HttpErrorResponse) {
+      // irl we would log this properly
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent) {
+          // client side network error
+          errorMessage = `And error occurred: ${err.error.message}`;
+      } else {
+          // backend returned unsuccessful code
+          // body may contain clues
+          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(errorMessage);
+  }
 }
